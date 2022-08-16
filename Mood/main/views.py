9d485bd22594549f8017ca6ca4a5999b470a3ef0
jf-puntocom <56199924+jf-puntocom@users.noreply.github.com>
2022-08-16 12:39:03 -0500

    Reconocimiento facial con cámara funcionando

--- Mood/main/views.py
+++ Mood/main/views.py
@@ -1,5 +1,9 @@
 from urllib import request
 from django.shortcuts import render
+import matplotlib.pyplot as plt
+import cv2
+from deepface import DeepFace
+import numpy as np
 
 # Create your views here.
 
@@ -8,4 +12,50 @@ def home_page(request): # Views para la home page
 
 
 def moood_page(request): #Views para la pagina mood
-    return render(request, template_name='mood.html')
+    userEmotion = ""
+    face_cascade_name = cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml'  #getting a haarcascade xml file
+    face_cascade = cv2.CascadeClassifier()  #processing it for our project
+    if not face_cascade.load(cv2.samples.findFile(face_cascade_name)):  #adding a fallback event
+        print("Error loading xml file")
+
+    video=cv2.VideoCapture(0)  #requisting the input from the webcam or camera
+
+    while video.isOpened():  #checking if are getting video feed and using it
+        _,frame = video.read()
+
+        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)  #changing the video to grayscale to make the face analisis work properly
+        face=face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5)
+
+        for x,y,w,h in face:
+            img=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),1)  #making a recentangle to show up and detect the face and setting it position and colour
+        
+            #making a try and except condition in case of any errors
+            try:
+                analyze = DeepFace.analyze(frame)  #same thing is happing here as the previous example, we are using the analyze class from deepface and using ‘frame’ as input
+                userEmotion = analyze['dominant_emotion']
+            #print(analyze['dominant_emotion'])  #here we will only go print out the dominant emotion also explained in the previous example
+            except:
+                userEmotion = " "
+
+            #this is the part where we display the output to the user
+            cv2.imshow('video', frame)
+            key=cv2.waitKey(1) 
+            if key==ord('q'):   # here we are specifying the key which will stop the loop and stop all the processes going
+                break
+        video.release()
+
+        if userEmotion == "happy":
+            userEmotion = "feliz"
+
+        elif userEmotion == "sad" or userEmotion == "fear":
+            userEmotion = "triste"
+
+        elif userEmotion == "angry" or userEmotion == "disgust":
+            userEmotion = "enojado"
+
+        elif userEmotion == "neutral" or userEmotion == "surprised":
+            userEmotion = "neutral"
+        else:
+            userEmotion = "No reconocio tu cara, vuelve a intentarlo"
+
+    return render(request, template_name='mood.html', context={"userEmotion": userEmotion,})
